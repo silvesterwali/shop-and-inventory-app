b004c222bcb27094e0577eb2360660eeaa784a53
silvesterwali <silvesterlhwali123@gmail.com>
2021-05-29 21:22:36 +0800

    adding admin page

---
+++ pages/admin/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Admin page',
+      meta: [
+        {
+          uid: 'description',
+          name: 'description',
+          content: 'Page for admin ',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/admin/users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'User Management',
+      meta: [
+        {
+          uid: 'description',
+          name: 'description',
+          content: 'User management page and list all user',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
