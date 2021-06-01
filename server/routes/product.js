ba26070250eb770c009960879e2c6cc40058fee7
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-01 17:41:31 +0800

    adding product service endpoint

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const product = require('../controllers/productController')
 router.get('/', product.getProducts)
 router.post('/', product.createProduct)
+router.get('/:productId', product.getProduct)
 router.put('/:productId', product.updateProduct)
 router.delete('/:productId', product.deleteProduct)
 module.exports = router
---
+++ services/product.js
@@ -0,0 +1,59 @@
+/**
+ * endpoint for access product resource
+ *
+ *
+ */
+
+import ApiClient from './apiClient.js'
+
+/**
+ * getProducts
+ *
+ * - endpoint to get all product resource
+ * @param {Number} limit
+ * @param {Number} page
+ */
+export const getProducts = async (limit, page) => {
+  return await ApiClient.get(`/api/products?limit=${limit}&page=${page}`)
+}
+
+/**
+ * createProduct
+ *
+ * - endpoint create new product resource
+ * @param {Object} payload
+ */
+export const createProduct = async (payload) => {
+  return await ApiClient.post(`/api/products`, payload)
+}
+
+/**
+ * getProduct
+ *
+ * - endpoint get specific product resource
+ * @param {String} productId
+ */
+export const getProduct = async (productId) => {
+  return await ApiClient.get(`/api/products/${productId}`)
+}
+
+/**
+ * updateProduct
+ *
+ * - endpoint to update specific product resource
+ * @param {String} productId
+ * @param {Object} payload
+ */
+export const updateProduct = async (productId, payload) => {
+  return await ApiClient.put(`/api/products/${productId}`, payload)
+}
+
+/**
+ * deleteProduct
+ *
+ * - endpoint delete specific product resource
+ * @param {String} productId
+ */
+export const deleteProduct = async (productId) => {
+  return await ApiClient.delete(`/api/products/${productId}`)
+}
