b1d91edaa26b7850c8648b422d425067d6a4bbae
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-03 18:31:05 +0800

    ad

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,9 +87,7 @@ export default {
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    baseURL: `http://localhost:${process.env.PORT || 3000}/`,
-  },
+  axios: {},
   loaders: {
     vue: {
       prettify: false,
