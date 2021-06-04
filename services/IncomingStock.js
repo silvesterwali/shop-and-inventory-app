22b0b1624bede1628894f6c52306ff02af6a28c9
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-05 00:48:27 +0800

    snipper fix

--- services/IncomingStock.js
+++ services/IncomingStock.js
@@ -8,64 +8,68 @@
  **/
 import ApiClient from './apiClient.js'
 
-/**
-* getResources
-*
-* - endpoint to get all incomingStock resource 
-* @param {Number} limit - limit resource to retrieve from backend
-* @param {Number} page - current page index for pagination
-* @async
+// endpoint for api endpoint
 
-*/
+const url = `incomingStock`
+
+/**
+ * getResources
+ *
+ * - endpoint to get all incomingStock resource
+ * @param {Number} limit - limit resource to retrieve from backend
+ * @param {Number} page - current page index for pagination
+ * @async
+ *
+ */
 export const getIncomingStockResources = async (limit, page) => {
-  return await ApiClient.get(`/incomingStock?limit=${limit}&page=${page}`)
+  return await ApiClient.get(`/${url}?limit=${limit}&page=${page}`)
 }
 
 /**
-* createResource
-*
-* - endpoint to create new incomingStock resource 
-* @param {Object} payload - data object for resource
-* @async
-
-*/
+ * createResource
+ *
+ * - endpoint to create new incomingStock resource
+ * @param {Object} payload - data object for resource
+ * @async
+ *
+ */
 export const createIncomingStockResource = async (payload) => {
-  return await ApiClient.post(`/incomingStock`, payload)
+  return await ApiClient.post(`/${url}`, payload)
 }
 
 /**
-* getResource
-*
-* - endpoint to take specific incomingStock resource 
-* @param {String} id
-* @async
-
-*/
+ * getResource
+ *
+ * - endpoint to take specific incomingStock resource
+ * @param {String} id
+ * @async
+ *
+ */
 export const getIncomingStockResource = async (id) => {
-  return await ApiClient.get(`/incomingStock/${id}`)
+  return await ApiClient.get(`/${url}/${id}`)
 }
 
 /**
-* updateResource
-*
-* - endpoint to update specific incomingStock resource 
-* @param {String} id - id of data resource
-* @param {Object} payload - data object for resource
-* @async
-
-*/
+ * updateResource
+ *
+ * - endpoint to update specific incomingStock resource
+ * @param {String} id - id of data resource
+ * @param {Object} payload - data object for resource
+ * @async
+ *
+ */
 export const updateIncomingStockResource = async (id, payload) => {
-  return await ApiClient.put(`/incomingStock/${id}`, payload)
+  return await ApiClient.put(`/${url}/${id}`, payload)
 }
 
 /**
-* deleteResource
-*
-* - endpoint to delete specific incomingStock resource 
-* @param {String} id
-* @async
-
-*/
+ * deleteResource
+ *
+ * - endpoint to delete specific incomingStock resource
+ * @param {String} id
+ * @async
+ *
+ */
 export const deleteIncomingStockResource = async (id) => {
-  return await ApiClient.delete(`/incomingStock/${id}`)
+  return await ApiClient.delete(`/${url}/${id}`)
 }
