ceb8c8847080806a99283c41404ccf7399fde205
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-05 23:14:11 +0800

    adding supplier basic table of supplier

--- components/Tables/Supplier/SupplierTable.vue
+++ components/Tables/Supplier/SupplierTable.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     <index-card-page>
-      <template #card-title>Example Title</template>
+      <template #card-title>Suppliers</template>
       <template #card-subtitle>
         <div>
-          <span>Example suptitle</span>
-          <v-btn color="primary" small class="mt-n5 float-right" to="/"
-            >Go To Somewhere</v-btn
+          <span>All supplier resource</span>
+          <v-btn
+            color="primary"
+            small
+            class="mt-n5 float-right"
+            to="/inventory/supplier/create"
+            >Create new</v-btn
           >
         </div>
       </template>
       <template #card-text>
         <!-- listed  component for page on this slot section -->
+        <v-data-table :headers="headers" :items="suppliers" dense>
+          <template #[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          </template>
+        </v-data-table>
       </template>
     </index-card-page>
   </div>
@@ -25,7 +37,41 @@ export default {
     IndexCardPage,
   },
   data() {
-    return {}
+    return {
+      headers: [
+        {
+          text: 'Name',
+          sortable: true,
+          align: 'start',
+          value: 'name',
+        },
+        {
+          text: 'Phone',
+          sortable: true,
+          align: 'start',
+          value: 'phone',
+        },
+        {
+          text: 'Email',
+          sortable: true,
+          align: 'start',
+          value: 'email',
+        },
+        {
+          text: 'Address',
+          sortable: true,
+          align: 'start',
+          value: 'address',
+        },
+        {
+          text: 'Actions',
+          sortable: true,
+          align: 'start',
+          value: 'action',
+        },
+      ],
+      suppliers: [],
+    }
   },
 }
 </script>
