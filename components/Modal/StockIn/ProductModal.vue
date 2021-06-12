39807a6196fa08c1964c5228b5916d3aac44b150
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-12 15:53:56 +0800

    update basic product modal

--- components/Modal/StockIn/ProductModal.vue
+++ components/Modal/StockIn/ProductModal.vue
@@ -1,77 +1,124 @@
 <template>
   <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">Item Stock</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="12" md="12">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Product"
-                  dense
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  dense
-                  label="qty"
-                  type="number"
-                  step="any"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field label="Unit" dense required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  type="number"
-                  step="any"
-                  label="price"
-                  dense
-                  required
-                ></v-text-field>
-              </v-col>
+      <v-form ref="form" @submit.prevent="validateForm">
+        <v-card :loading="loading">
+          <v-card-title>
+            <span class="text-h5">Item Stock</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="12" md="12">
+                  <v-select
+                    v-model="dataForm.productId"
+                    :items="productList"
+                    label="Product"
+                    item-value="_id"
+                    item-text="name"
+                    dense
+                    :rules="[(v) => !!v || 'product is required']"
+                    required
+                    @input="setUnitToNull"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="dataForm.qty"
+                    dense
+                    label="qty"
+                    type="number"
+                    :rules="[(v) => !!v || 'qty  is required']"
+                    step="any"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="dataForm.unit"
+                    label="Unit"
+                    :rules="[(v) => !!v || 'unit is required']"
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="dataForm.price"
+                    type="number"
+                    step="any"
+                    :rules="[(v) => !!v || 'price is required']"
+                    label="price"
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  label="discount"
-                  dense
-                  type="number"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <v-textarea dense label="description" rows="2"></v-textarea>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Close
-          </v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="dataForm.discount"
+                    label="discount"
+                    dense
+                    :rules="[(v) => !!v || 'discount is required']"
+                    type="number"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="12" md="12">
+                  <v-textarea
+                    v-model="dataForm.description"
+                    dense
+                    label="description"
+                    rows="2"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              type="button"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text type="submit"> Save </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
   </v-row>
 </template>
 <script>
+import { getProductListResources } from '@/services/productList.js'
 export default {
   props: {
     openDialog: {
       type: Boolean,
       default: false,
     },
+  },
+  data() {
+    return {
+      // object for form
+      dataForm: {
+        _id: null,
+        productId: null,
+        qty: 0,
+        unit: null,
+        price: 0,
+        discount: 0,
+        description: null,
+      },
+      productList: [], // list product
+    }
+  },
+  async fetch() {
+    const { data } = await getProductListResources()
+    this.productList = data
   },
 
   computed: {
@@ -82,6 +129,41 @@ export default {
       get() {
         return this.openDialog
       },
+    },
+    loading() {
+      return this.$fetchState
+    },
+    /**
+     * units
+     * - determine if all product have their own unit
+     * - the unit will fire from selected product unit
+     */
+    units() {
+      if (this.dataForm.productId === null) {
+        return []
+      }
+      // just find out the unit array from selected product
+      const productUnit = this.productList.find(
+        (el) => el._id === this.dataForm.productId
+      )
+      return productUnit.unit
+    },
+  },
+  methods: {
+    /**
+     * setUnitToNull
+     *
+     * - method will call on input the product
+     * - this will change the dataForm unit to null , so user will fill new product unit
+     */
+    setUnitToNull() {
+      this.dataForm.unit = null
+    },
+    // method will validate the form before sended the data to backend
+    validateForm() {
+      if (!this.$refs.form.validate()) {
+        return false
+      }
     },
   },
 }
