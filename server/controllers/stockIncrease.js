c85c20f07f1c6e10b592cf2631dfd95ca577b0a1
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-13 22:03:04 +0800

    adding stock increment method

--- server/controllers/stockIncrease.js
+++ server/controllers/stockIncrease.js
@@ -5,6 +5,9 @@
  * - will change the status to 1 (status effect the qty stock of product)
  **/
 
+const ObjectId = require('mongodb').ObjectID
+const db = require('../db').db
+
 /**
  *=====================================
  * update
@@ -16,10 +19,29 @@
  * @param {express.Response} res
  * @async
  **/
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   /**
    * - 1 take incomingStock by _id
    * - 2 validate if the incomingStock has productsInTransaction item more then one
    *
    */
+
+  try {
+    const stock = await db
+      .collection('IncomingStocks')
+      .findOne({ _id: new ObjectId(req.params.id) })
+
+    if (!stock) {
+      return res.status(404).message({ message: 'Data not found' })
+    }
+
+    const { productsInTransactions } = stock
+
+    if (productsInTransactions.length <= 0) {
+      // prevent if product in transaction is empty
+      return res.status(422).json({
+        message: 'There no item product in transaction list. Operation failed',
+      })
+    }
+  } catch (err) {}
 }
---
+++ server/utilities/stok.js
@@ -0,0 +1,26 @@
+const ObjectID = require('mongodb').ObjectID
+const db = require('../db').db
+/**
+ * incrementStockProduct
+ *
+ *
+ * - this method will responsible to increase a product stock
+ * @param {String} productId
+ * @param {Number} qty
+ * @async
+ */
+exports.incrementStockProduct = async (productId, qty) => {
+  try {
+    await db.collection('products').updateOne(
+      { _id: new ObjectID(productId) },
+      {
+        $inc: {
+          stockQty: parseFloat(qty),
+        },
+      }
+    )
+    return true
+  } catch (err) {
+    throw new Error(err)
+  }
+}
