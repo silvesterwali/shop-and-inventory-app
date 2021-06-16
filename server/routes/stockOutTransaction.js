1b93c883021dc064f207f0efff9e2f35a3ee120f
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-16 23:09:06 +0800

    create stockout transaction router

---
+++ server/routes/stockOutTransaction.js
@@ -0,0 +1,24 @@
+/**
+ *=============================================
+ * - @copyright 2021
+ *
+ * router
+ *
+ *
+ *
+ *
+ *
+ **/
+const express = require('express')
+const auth = require('../middleware/auth')
+const router = express.Router()
+const StockOutTransaction = require('../controllers/StockOutController')
+
+router.use(auth)
+router.get('/', StockOutTransaction.index)
+router.post('/', StockOutTransaction.store)
+router.get('/:id', StockOutTransaction.show)
+router.put('/:id', StockOutTransaction.update)
+router.delete('/:id', StockOutTransaction.destroy)
+
+module.exports = router
