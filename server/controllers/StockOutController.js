e806cfe0b89074246c87c4a8b304a6b431dac5c7
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-16 22:30:12 +0800

    adding logic to store and store

--- server/controllers/StockOutController.js
+++ server/controllers/StockOutController.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-console */
 /**
  * @copyright 2021
  *
  *
  **/
+
+const ObjectID = require('mongodb').ObjectID
+const db = require('../db').db
+const stockNumber = require('../utilities/stockNumber.js')
 
 /**
  *=====================================
@@ -14,7 +19,7 @@
  * @param {express.Response} res
  * @async
  **/
-exports.index = (req, res) => {}
+exports.index = async (req, res) => {}
 
 /**
  *=====================================
@@ -26,7 +31,30 @@ exports.index = (req, res) => {}
  * @param {express.Response} res
  * @async
  **/
-exports.store = (req, res) => {}
+exports.store = async (req, res) => {
+  const { type, description, transactionDate } = req.body
+
+  try {
+    const result = await db.collection('stockOutTransactions').insertOne({
+      _id: new ObjectID(),
+      serialNumber: await stockNumber.stockOutTransactionNumber(),
+      transactionDate,
+      description,
+      type, // to define the type of stock out transaction [production ,return]
+      status: 0, // user pending,
+      createdBy: new ObjectID(req.user._id),
+      createdAt: new Date(),
+      productsInTransactions: [],
+    })
+    return res.json({
+      message: 'Success create new stock out transaction',
+      result: result.ops[0],
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
 
 /**
  *=====================================
@@ -51,7 +79,34 @@ exports.show = (req, res) => {}
  * @param {express.Response} res
  * @async
  **/
-exports.update = (req, res) => {}
+exports.update = async (req, res) => {
+  const { type, description, transactionDate } = req.body
+
+  try {
+    const result = await db.collection('stockOutTransactions').updateOne(
+      {
+        _id: new ObjectID(req.params.id),
+      },
+      {
+        $set: {
+          transactionDate,
+          description,
+          type, // to define the type of stock out transaction [production ,return]
+          status: 0, // user pending,
+          updatedBy: new ObjectID(req.user._id),
+          updatedAt: new Date(),
+        },
+      }
+    )
+    return res.json({
+      message: 'Success update current stock out transaction',
+      result: result.ops[0],
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
 
 /**
  *=====================================
