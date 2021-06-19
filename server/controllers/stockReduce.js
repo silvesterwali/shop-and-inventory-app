52ab580a460253f3d5d243a2d071c74d0d9d7e30
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-19 22:07:52 +0800

    create stock reduce js

---
+++ server/controllers/stockReduce.js
@@ -0,0 +1,22 @@
+/**
+ * @copyright 2021
+ * - this controller is responsible to reduce the product according the
+ * stock out transaction
+ * - this only handle approver from the stock out transaction approve
+ * - before to reduce the stock of every product it will some basic validate
+ * to make that every product have enough stock qty before
+ * - if the product does not  meet the requirement will impact for whole transaction to canceled
+ **/
+
+/**
+ *=====================================
+ * update
+ *
+ *
+ * - update the specific resource
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.update = (req, res) => {}
