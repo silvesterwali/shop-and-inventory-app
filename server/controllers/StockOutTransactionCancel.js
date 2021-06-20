33e433d1af3c773f7a9a2c98e735f1b66210a0ae
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-20 11:46:53 +0800

    adding stock out transaction cancellation

---
+++ server/controllers/StockOutTransactionCancel.js
@@ -0,0 +1,18 @@
+/**
+ * @copyright 2021
+ * - this controller is responsible for cancel any stock out transaction
+ * - sometime the transaction can cancel in any status except already canceled before
+ **/
+
+/**
+ *=====================================
+ * update
+ *
+ *
+ * - update the specific resource
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.update = (req, res) => {}
