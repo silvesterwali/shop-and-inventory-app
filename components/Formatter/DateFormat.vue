ff2a746ab3c984c45353553a1a795b05413285de
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-21 22:38:55 +0800

    adding date format

---
+++ components/Formatter/DateFormat.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>{{ date }}</span>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  props: {
+    dateString: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    date() {
+      if (this.dateString === null) {
+        return moment().format('YYYY-MM-DD')
+      }
+      return moment(this.dateString).format('YYYY-MM-DD')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
