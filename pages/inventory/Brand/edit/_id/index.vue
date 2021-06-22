0e2fb2286b3b7c93d28ba2f02bb75d0b91597e8b
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-23 00:22:18 +0800

    adding brand page

---
+++ pages/inventory/Brand/edit/_id/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <index-base-page>
+      <template #info>
+        <v-card>
+          <v-card-text>
+            {{ pageDescription }}
+          </v-card-text>
+        </v-card>
+      </template>
+      <template #content>
+        <!-- page page will be here -->
+      </template>
+    </index-base-page>
+  </div>
+</template>
+
+<script>
+import IndexBasePage from '@/components/BasePage/IndexBasePage.vue'
+
+export default {
+  components: {
+    // define your component here
+    IndexBasePage,
+  },
+  data() {
+    return {
+      pageTitle: `Page-index`,
+      pageDescription:
+        'this page is under develop, please provide some feedback to improve it for better documentation',
+    }
+  },
+  head() {
+    // this property for meta tag
+    return {
+      title: this.pageTitle,
+      meta: [
+        {
+          uid: 'description',
+          name: 'description',
+          content: this.pageDescription,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
