a07ac598f820d72267de6fe930580f7dc5cd2412
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-22 20:25:32 +0800

    adding new customer number

---
+++ server/utilities/customerNumberUtils.js
@@ -0,0 +1,61 @@
+const moment = require('moment')
+const db = require('../db').db
+/**
+ * customerSerialNumberGenerate
+ *
+ * - create customer serial number
+ * @return {String}
+ */
+exports.customerSerialNumberGenerate = async () => {
+  const valueNumber = await db
+    .collection('customers')
+    .aggregate([
+      {
+        $addFields: {
+          last_index: {
+            $substr: [
+              '$serialNumber',
+              {
+                $subtract: [
+                  {
+                    $strLenCP: '$serialNumber',
+                  },
+                  6,
+                ],
+              },
+              6,
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          lastGreat: {
+            $max: { $toInt: '$last_index' },
+          },
+        },
+      },
+      {
+        $sort: {
+          lastGreat: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ])
+    .toArray()
+  const lastNumber =
+    valueNumber.length > 0 ? parseInt(valueNumber[0].lastGreat) : 0
+  const newNumber = lastNumber + 1
+  const strLength = newNumber.toString().length
+  let _stringFormat = ''
+  for (let i = 6; i > 1; i++) {
+    if (strLength === i) {
+      break
+    }
+    _stringFormat += '0'.toString()
+  }
+
+  return 'CT' + moment().format('YYYY') + _stringFormat + newNumber.toString()
+}
