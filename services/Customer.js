fc94bf62c448c0bc68e380a671372a25473f9e0d
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-22 20:05:49 +0800

    adding customer services

---
+++ services/Customer.js
@@ -0,0 +1,73 @@
+/**
+ * @copyright 2021
+ * - service endpoint for Customer
+ *
+ *
+ *
+ *
+ **/
+import ApiClient from './apiClient.js'
+
+// endpoint for api endpoint
+
+const url = `api/customer`
+
+/**
+ * getCustomerResources
+ *
+ * - endpoint to get all Customer resource
+ * @async
+ *
+ */
+export const getCustomerResources = async (limit, page) => {
+  return await ApiClient.get(`/${url}`)
+}
+
+/**
+ * createCustomerResource
+ *
+ * - endpoint to create new Customer resource
+ * @param {Object} payload - data object for resource
+ * @async
+ *
+ */
+export const createCustomerResource = async (payload) => {
+  return await ApiClient.post(`/${url}`, payload)
+}
+
+/**
+ * getCustomerResource
+ *
+ * - endpoint to take specific Customer resource
+ * @param {String} id
+ * @async
+ *
+ */
+export const getCustomerResource = async (id) => {
+  return await ApiClient.get(`/${url}/${id}`)
+}
+
+/**
+ * updateCustomerResource
+ *
+ * - endpoint to update specific Customer resource
+ * @param {String} id - id of data resource
+ * @param {Object} payload - data object for resource
+ * @async
+ *
+ */
+export const updateCustomerResource = async (id, payload) => {
+  return await ApiClient.put(`/${url}/${id}`, payload)
+}
+
+/**
+ * deleteCustomerResource
+ *
+ * - endpoint to delete specific Customer resource
+ * @param {String} id
+ * @async
+ *
+ */
+export const deleteCustomerResource = async (id) => {
+  return await ApiClient.delete(`/${url}/${id}`)
+}
