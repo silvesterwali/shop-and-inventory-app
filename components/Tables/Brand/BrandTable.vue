5daa17e90f6871e25d5dffcd41eab87dbb253e62
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-23 21:54:59 +0800

    fix snippet table

---
+++ components/Tables/Brand/BrandTable.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :loading="fetchState.pending"
+      class="elevation-1"
+    >
+      <template #top>
+        <v-toolbar flat>
+          <v-toolbar-title>Item management</v-toolbar-title>
+          <v-devider class="mt-4" inset vertical />
+          <v-btn color="primary" class="mb-2">New Item</v-btn>
+        </v-toolbar>
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click.prevent="editItem(item)"
+          >mdi-pencil</v-icon
+        >
+        <v-icon small class="mr-2" @click.prevent="deleteItem(item)"
+          >mdi-delete</v-icon
+        >
+      </template>
+    </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headres: [
+        {
+          text: 'name',
+          value: 'name',
+        },
+        {
+          text: 'email',
+          value: 'email',
+        },
+        {
+          text: 'actions',
+          value: 'actions',
+          sortable: false,
+        },
+      ],
+      items: [],
+      selectedItem: null,
+      deleteDialog: false,
+      editDialog: false,
+    }
+  },
+  fetch() {},
+  methods: {
+    /**
+     * deleteItem
+     **/
+    deleteItem(item) {
+      this.selectedItem = item
+      this.deleteDialog = true
+    },
+    /**
+     * editItem
+     **/
+    editItem(item) {
+      this.selectedItem = item
+      this.editDialog = true
+    },
+  },
+}
+</script>
+<style lang="scss" scoped></style>
