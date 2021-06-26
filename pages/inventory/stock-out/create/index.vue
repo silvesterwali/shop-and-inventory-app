a73f082f58d10654cd84319eeb791974fb55d2f7
silvesterwali <silvesterlhwali123@gmail.com>
2021-06-26 22:48:20 +0800

    refactor index create stock out

--- pages/inventory/stock-out/create/index.vue
+++ pages/inventory/stock-out/create/index.vue
@@ -10,7 +10,20 @@
       </template>
       <template #content>
         <!-- page page will be here -->
-        <head-stock-out-form />
+        <card-toolbar-page>
+          <template #title>Create Stock Out</template>
+          <template #action>
+            <toolbar-nav
+              return-url="/inventory/stock-out"
+              :add-action="false"
+            />
+          </template>
+          <template #content>
+            <v-card-text>
+              <head-stock-out-form />
+            </v-card-text>
+          </template>
+        </card-toolbar-page>
       </template>
     </index-base-page>
   </div>
@@ -19,12 +32,15 @@
 <script>
 import IndexBasePage from '@/components/BasePage/IndexBasePage.vue'
 import HeadStockOutForm from '@/components/Forms/StockOut/HeadStockOutForm.vue'
-
+import CardToolbarPage from '~/components/CardPage/CardToolbarPage.vue'
+import ToolbarNav from '~/components/Nav/Toolbar/ToolbarNav.vue'
 export default {
   components: {
     // define your component here
     IndexBasePage,
     HeadStockOutForm,
+    CardToolbarPage,
+    ToolbarNav,
   },
   data() {
     return {
