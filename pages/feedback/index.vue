ee9fd2c60a205ef8e258e074f1f7e6784ed15891
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-03 19:36:10 +0800

    adding feedback page

---
+++ pages/feedback/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-container>
+      <v-row no-gutters justify="center" align="center" class="my-10">
+        <v-col cols="12" md="8" lg="8" class="my-5">
+          <CardToolbarPage>
+            <template #title>Feedback</template>
+            <template #content>
+              <v-card-text>
+                <FeedbackForm redirect-url="/" />
+              </v-card-text>
+            </template>
+          </CardToolbarPage>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import FeedbackForm from '~/components/Forms/Feedback/FeedbackForm.vue'
+import CardToolbarPage from '~/components/CardPage/CardToolbarPage.vue'
+export default {
+  components: {
+    FeedbackForm,
+    CardToolbarPage,
+  },
+  layout: 'feedback',
+  auth: false,
+  data() {
+    return {
+      pageTitle: `Page-index`,
+      pageDescription:
+        'this page is under develop, please provide some feedback to improve it for better documentation',
+    }
+  },
+  head() {
+    // this property for meta tag
+    return {
+      title: this.pageTitle,
+      meta: [
+        {
+          uid: 'description',
+          name: 'description',
+          content: this.pageDescription,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
