35dab5f4711b0124d803e97b0dff56ce12f0e488
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-03 22:49:36 +0800

    create item dashboard

---
+++ components/Card/ItemDashboardCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card class="p-0">
+    <div class="d-flex flex-no-wrap justify-space-arround">
+      <v-avatar color="yellow lighten-3" tile size="64" class="mx-1 my-auto">
+        <v-icon color="red" aria-hidden="false" x-large>{{ icon }}</v-icon>
+      </v-avatar>
+      <div>
+        <v-card-title class="text-h5"> {{ cardTitle }} </v-card-title>
+        <v-card-subtitle>{{ cardSubTitle }}</v-card-subtitle>
+      </div>
+    </div>
+    <v-divider />
+    <v-card-actions>
+      <v-btn text small :to="url">{{ btnText }}</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: 'url',
+    },
+    btnText: {
+      type: String,
+      default: 'Go Somewhere',
+    },
+    cardTitle: {
+      type: String,
+      default: 'Card title',
+    },
+    cardSubTitle: {
+      type: String,
+      default: 'Card title',
+    },
+    icon: {
+      type: String,
+      default: 'mdi-cube-outline',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="(item, index) in items"
+        :key="index"
+        class="12"
+        lg="3"
+        md="3"
+        sm="4"
+      >
+        <ItemDashboardCard v-bind="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ItemDashboardCard from '~/components/Card/ItemDashboardCard.vue'
+export default {
+  components: {
+    ItemDashboardCard,
+  },
+  data() {
+    return {
+      items: [
+        {
+          url: '/inventory/product',
+          btnText: 'Go To Products',
+          cardTitle: '5000',
+          cardSubTitle: 'Product',
+          icon: 'mdi-cube-outline',
+        },
+        {
+          url: '/inventory/brand',
+          btnText: 'Go To Brands',
+          cardTitle: '50',
+          cardSubTitle: 'Brands',
+          icon: 'mdi-apps',
+        },
+        {
+          url: '/inventory/customer',
+          btnText: 'Go To Customer',
+          cardTitle: '50',
+          cardSubTitle: 'Customer',
+          icon: 'mdi-account-group',
+        },
+        {
+          url: '/admin/users',
+          btnText: 'Go To Users',
+          cardTitle: '50',
+          cardSubTitle: 'User',
+          icon: 'mdi-account-group',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
