1ce8eacc9c5784b7b3813e2ea64b38dc64243350
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-04 10:10:21 +0800

    adding item dashboard route

---
+++ server/routes/itemDashboardRoute.js
@@ -0,0 +1,20 @@
+/**
+ *=============================================
+ * - @copyright 2021
+ *
+ * router
+ *
+ *
+ *
+ *
+ *
+ **/
+
+const express = require('express')
+const router = express.Router()
+const ItemDashboard = require('../controllers/ItemDashboard')
+const auth = require('../middleware/auth')
+router.use(auth)
+router.get('/', ItemDashboard.index)
+
+module.exports = router
