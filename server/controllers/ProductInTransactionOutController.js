55b5c98ce647609522e97bbeaa236bf4481566f1
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-07 22:13:10 +0800

    making product transaction out

---
+++ server/controllers/ProductInTransactionOutController.js
@@ -0,0 +1,68 @@
+/**
+ * @copyright 2021
+ *
+ * - this controller is provide the qty of product
+ * - in transaction out
+ *
+ **/
+
+/**
+ *=====================================
+ * index
+ *
+ * - get listing of resources
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.index = (req, res) => {}
+
+/**
+ *=====================================
+ * store
+ *
+ * - store new resource to storage
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.store = (req, res) => {}
+
+/**
+ *=====================================
+ * show
+ *
+ * - show the specific of resource from storage
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.show = (req, res) => {}
+
+/**
+ *=====================================
+ * update
+ *
+ *
+ * - update the specific resource
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.update = (req, res) => {}
+
+/**
+ *=====================================
+ * destroy
+ *
+ * - destroy specific resource from storage
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @async
+ **/
+exports.destroy = (req, res) => {}
