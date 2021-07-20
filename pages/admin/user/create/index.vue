0927ac09b9e40619a4e1f8a1016cba419cbb51f6
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-20 21:12:31 +0800

    save it

---
+++ pages/admin/user/create/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <IndexBasePage>
+    <template #info>
+      <v-card>
+        <v-card-text>
+          {{ pageInfo.content }}
+        </v-card-text>
+      </v-card>
+    </template>
+    <template #content>
+      <CardToolbarPage>
+        <template #title>User's</template>
+        <template #action>
+          <ToolbarNav :add-action="false" return-url="/admin/user" />
+        </template>
+        <template #content> </template>
+      </CardToolbarPage>
+    </template>
+  </IndexBasePage>
+</template>
+
+<script>
+import IndexBasePage from '@/components/BasePage/IndexBasePage.vue'
+import CardToolbarPage from '~/components/CardPage/CardToolbarPage.vue'
+import ToolbarNav from '~/components/Nav/Toolbar/ToolbarNav.vue'
+export default {
+  components: {
+    IndexBasePage,
+    CardToolbarPage,
+    ToolbarNav,
+  },
+  data() {
+    return {
+      pageInfo: {
+        content: 'User management page and list all user',
+      },
+    }
+  },
+  head() {
+    return {
+      title: 'User Management',
+      meta: [
+        {
+          uid: 'description',
+          name: 'description',
+          content: this.pageInfo.content,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
