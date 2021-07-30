596bcf8860249a83a8d949761167826b8f295bb5
silvesterwali <silvesterlhwali123@gmail.com>
2021-07-30 23:42:06 +0800

    adding setting vscode for autofix eslint prettier issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,11 @@ module.exports = {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-container>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem
+      aspernatur ipsa totam. Vero, dignissimos dolorum! Consequuntur dolorem
+      dolore officia nulla? Quo enim odit repellendus ipsa minus, deleniti
+      nostrum qui inventore!
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
