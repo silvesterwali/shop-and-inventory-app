a512ffc080354ca125fcad464f2238fd5947467e
silvesterwali <silvesterlhwali123@gmail.com>
2021-08-17 10:40:04 +0800

    adding todos modal adn page

---
+++ components/Modal/Todo/TodoModal.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="400">
+      <v-form ref="form" @submit.prevent="validateForm">
+        <v-card :loading="loading">
+          <v-card-title>
+            {{ dataForm._id !== null ? 'Update' : 'Create' }} Todo
+          </v-card-title>
+          <v-card-text>
+            <v-col cols="12" lg="12" sm="12" md="12">
+              <v-textarea
+                v-model="dataForm.description"
+                rows="2"
+                label="Todo"
+                :rules="[(v) => !!v || 'todo is required']"
+              />
+            </v-col>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              type="button"
+              color="red darken-1"
+              text
+              @click="dialog = false"
+              >No</v-btn
+            >
+            <v-spacer />
+            <v-btn type="submit" color="green darken-1" text>Submit</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+import setMessage from '@/mixins/setMessage.js'
+import { createTodoResource, updateTodoResource } from '~/services/Todo.js'
+export default {
+  mixins: [setMessage],
+  props: {
+    // control the ***dialog*** with this props
+    dialogTodo: {
+      type: Boolean,
+      default: false,
+    },
+    // previous todo object for update
+    todo: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      loading: false,
+      dataForm: {
+        _id: null,
+        description: null,
+        status: 'In Progress',
+      },
+      errors: null,
+    }
+  },
+  computed: {
+    // make sure  that ***dialogTodo*** is using ***sync*** props
+    dialog: {
+      set(value) {
+        this.$emit('update:dialogTodo', value)
+      },
+      get() {
+        return this.dialogTodo
+      },
+    },
+  },
+  watch: {
+    // if todos props is not null thats mean the data will be update
+    todo: {
+      immediate: true,
+      handler(value) {
+        if (value !== null) {
+          this.dataForm = value
+        }
+      },
+    },
+  },
+  methods: {
+    validateForm() {
+      if (this.$refs.form.validate()) {
+        if (this.dataForm._id === null) {
+          this.sendCreate()
+        } else {
+          this.sendCreate()
+        }
+      }
+    },
+    async sendCreate() {
+      try {
+        const { data } = await createTodoResource(this.dataForm)
+        this.SET_MESSAGE({ text: data.message, color: 'success' })
+        this.dialog = false
+      } catch (err) {
+        this.errors = err.response.data
+      }
+    },
+    async sendUpdate() {
+      try {
+        const { data } = await updateTodoResource(
+          this.dataForm._id,
+          this.dataForm
+        )
+        this.SET_MESSAGE({ text: data.message, color: 'success' })
+        this.dialog = false
+      } catch ({ response }) {
+        this.errors = response.data
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/todos/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <IndexCardPage>
+      <template #card-title>Todo</template>
+      <template #card-subtitle>
+        <div>
+          <span>Create Your todo</span>
+        </div>
+      </template>
+      <template #card-text>
+        <!-- listed  component for page on this slot section -->
+      </template>
+    </IndexCardPage>
+  </div>
+</template>
+
+<script>
+import IndexCardPage from '@/components/CardPage/IndexCardPage.vue'
+export default {
+  components: {
+    // register component here
+    IndexCardPage,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
